31f11f88a1d82e466478d75fa846da4c3901b3ce
codedragon <codedragon@tistory.com>
2019-03-21 15:35:17 +0900

    commit3

--- app/src/main/java/com/tistory/codedragon/pp/MainActivity.java
+++ app/src/main/java/com/tistory/codedragon/pp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tistory.codedragon.pp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = new Intent(this, SplashActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/tistory/codedragon/pp/SplashActivity.java
+++ app/src/main/java/com/tistory/codedragon/pp/SplashActivity.java
@@ -1,22 +1,25 @@
 package com.tistory.codedragon.pp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout2);
-
-        try{
-            Thread.sleep(5000);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        startLoading();
     }
+
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 3000);
+    }
+
 }
